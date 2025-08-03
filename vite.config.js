cc721b428e58d98683f4131cfcdc8d0ecb1ad0df
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2025-08-03 23:24:34 +0900

    ipo_vue 20250803_03 - modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 이 부분이 추가되거나 수정된 부분입니다.
+  // 'ipo_vue'는 GitHub 저장소 이름과 동일해야 합니다.
+  base: '/ipo_vue/',
+
   optimizeDeps: {
     include: ['vue-cal']
   },
